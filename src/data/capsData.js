c0158b69ff38113a1451836f1719d6191abf6d84
EnzoBeltran <egbeltran01@gmail.com>
2025-03-08 13:37:48 -0300

    commit0803

--- src/data/capsData.js
+++ src/data/capsData.js
@@ -64,11 +64,22 @@ export const capsData = [
     nombre: "PEDRO PIZÁ",
     ubicacion:"Barker",
     direccion: "Guames 268",
-    enfermera: "Dora Noguez",
-    celular: "2281 - 358346",
+    enfermera: "Coordinador Sanches Nicolas",
+    celular: "2281 - 683979",
     profesionales: [
-      { nombre: "Lic. Figueroa Paz", especialidad: "Nutricionista" },
-      { nombre: "Santangelo Maria", especialidad: "Psicologia" },
+      { nombre: "Musciati Hernan", especialidad: "Cardiologo" },
+      { nombre: "Arapayu Damian", especialidad: "Clinica Medica" },
+      { nombre: "Soto Lorena", especialidad: "Clinica Medica" },
+      { nombre: "Olivera Silvina", especialidad: "Psiquiatria" },
+      { nombre: "Gongora Valentina", especialidad: "Fonoaudiologia" },
+      { nombre: "Garrido Estefania", especialidad: "Pediatra" },
+      { nombre: "Donofrio Juliana", especialidad: "Odontologa" },
+      { nombre: "Rebolo Maria", especialidad: "Odontologia" },
+      { nombre: "Malicia Betiana", especialidad: "Obstetricia" },
+      { nombre: "Zunino Santiago", especialidad: "Otorrinonaringologia" },
+      { nombre: "Cufre Miguel", especialidad: "Urologia" },
+      { nombre: "Salvatierra Bruno", especialidad: "Bioquimico" },
+      { nombre: "Cabrera Erica - Carrizo Alejandra - Pagola Virginia - Oliva Daiana - Berruti Daiana - Gomez Valentina", especialidad: "Enfermeria" },
     ]
   },
   {
@@ -76,11 +87,18 @@ export const capsData = [
     nombre: "FELIPE ANUNCIBAY",
     ubicacion:"Villa Cacique",
     direccion: "Guames 268",
-    enfermera: "Dora Noguez",
-    celular: "2281 - 358346",
+    enfermera: "Coordinador Sanchez Nicolas",
+    celular: "2292 - 498181",
     profesionales: [
-      { nombre: "Lic. Figueroa Paz", especialidad: "Nutricionista" },
-      { nombre: "Santangelo Maria", especialidad: "Psicologia" },
+      { nombre: "Nucciarone Rodolfo - Soto Lorena - Arapayu", especialidad: "Clinica Medica" },
+      { nombre: "San Rroqueondo Maria", especialidad: "Psicologia" },
+      { nombre: "Landa Lucia - Ganim Melisa", especialidad: "Fonoaudiologia" },
+      { nombre: "Rebolo Maria", especialidad: "Odontologia" },
+      { nombre: "Johnston Guadalupe", especialidad: "Nutricion" },
+      { nombre: "Kruger Elizabeth", especialidad: "Obstetricia" },
+      { nombre: "Garrido Estefania", especialidad: "Pediatria" },
+      { nombre: "Guerrero Sonia - Paz Margarita - Valdez Martina - Cordoba Marisa - Mendez Ana", especialidad: "Enfermeria" },
+      
     ]
   },
   {
@@ -132,16 +150,29 @@ export const capsData = [
     ]
   },
   {
-    id: 11,
-    nombre: "RIGANTI",
-    ubicacion:"Centrico",
-    direccion: "Zibecchi 74",
-    enfermera: "Dora Noguez",
-    celular: "2281 - 358346",
+    id: 12,
+    nombre: "RIGANTI", // Reemplázalo con el nombre correspondiente
+    ubicacion: "Centrico",
+    direccion: "Zibechi 74",
+    enfermera: "", // Si tienes este dato
+    celular: "2292 - 452002 ", // Si tienes este dato
     profesionales: [
-      { nombre: "Lic. Figueroa Paz", especialidad: "Nutricionista" },
-      { nombre: "Santangelo Maria", especialidad: "Psicologia" },
+        { nombre: "Bernaola Julieta - Borchi Marcela - Madero Ludmila - Yacob Elda", especialidad: "Enfermería y Vacunación" },
+        { nombre: "Góngora Valentina - Landa Lucía", especialidad: "Fonoaudiología" },
+        { nombre: "Ganim Melisa", especialidad: "Fonoaudiología (Sólo OEA)" },
+        { nombre: "Cernutto Martina - Donofrio Carla", especialidad: "Psicopedagogía" },
+        { nombre: "Araujo Paola - Galeano Yanina - Sarramone M. José", especialidad: "Odontología" },
+        { nombre: "Sabatte Natalia", especialidad: "Psicología" },
+        { nombre: "Ibáñez Daniela - Francischelli Gisela", especialidad: "Kinesiología" },
+        { nombre: "Kruger Elizabeth - Malizia Betiana - Von Kaven Sara", especialidad: "Obstetricia" },
+        { nombre: "Figueroa M. Paz - Jhonston Guadalupe", especialidad: "Nutrición" },
+        { nombre: "Antonucci Julieta", especialidad: "Clínica Médica" },
+        { nombre: "Del Rio Norberto", especialidad: "Ginecología" },
+        { nombre: "Rodriguez Jesús", especialidad: "Musicoterapia" },
+        { nombre: "Musciatti Hernán - Vazquez Miguel", especialidad: "Cardiología" },
+        { nombre: "Benavente Carlos - Ballent Andrés", especialidad: "Psiquiatría" },
+        { nombre: "Garrido Estefanía - Murua Paula - Laplace Gonzalo", especialidad: "Pediatría" }
     ]
-  }
+}
   // Agregar más CAPS aquí...
 ];
